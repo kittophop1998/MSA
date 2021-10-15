90c6c0b677c082b41f5e4936829e789d1c756693
kittiphop <kittiphopsompuech2@gmail.com>
2021-10-15 15:42:54 +0700

    update

--- resources/views/stock_item/stock.blade.php
+++ resources/views/stock_item/stock.blade.php
@@ -166,7 +166,7 @@
 <html>
 
 <head>
-    <title>Clean Streets - Style Guide</title>
+    <title>MSA</title>
     <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
     <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/scripts.js"></script>
